6535eaadb20f85236cf8c3ca76592f1dfea98626
unknown <J4sumit@gmail.com>
2023-05-04 14:07:17 +0530

    mongodb setup and expressJS server created

---
+++ server.js
@@ -0,0 +1,37 @@
+// const express = require('express');
+// const colors = require('colors');
+
+import express from 'express';
+import colors from 'colors';
+import dotenv from  'dotenv';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
+
+// configure env
+dotenv.config()
+
+
+// database configs
+connectDB();
+
+// rest object
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(morgan('dev'))
+
+
+//rest api
+
+app.get('/', (req, res)=> {
+    res.send('<h1>welocome to ecommerce project</h1>');
+})
+
+//port
+const PORT = process.env.PORT || 8080;
+
+//RUN LISTEN
+    app.listen(PORT, ()=>{
+        console.log(`server running on mode ${process.env.DEV_MODE} and PORT on ${PORT}`.bgCyan.white)
+    })
