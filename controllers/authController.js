3c2549330b9c40a980c81e47148c7d6a76de720f
unknown <J4sumit@gmail.com>
2023-05-04 22:22:03 +0530

    models added

---
+++ controllers/authController.js
@@ -0,0 +1,63 @@
+
+// import {userModel} from "../models/userModel.js";
+import {hashPassword} from "../helpers/authHelper.js";
+
+export const registerController = async (req, res) =>{
+    try{
+const {name, email, password, phone, address} = req.body;
+//validation 
+
+if(!name)
+{
+    return res.send({error: "Name is required"});
+}
+if(!email)
+{
+    return res.send({error: "Email is required"});
+}
+if(!password)
+{
+    return res.send({error: "Passwoed is required"});
+}
+if(!phone)
+{
+    return res.send({error: "phone number is required"});
+}
+if(!address)
+{
+    return res.send({error: "assress  is required"});
+}
+
+
+//check user
+
+const existingUser = await userModel.findOne({email});
+
+// Existing user
+ if(existingUser){
+     return res.status(200).send({
+         success:true,
+         messange : "Already Register please login",
+     })
+ }   
+ //register User
+ const hashedPassword = await hashPassword(passwoed);
+ //save
+ const user = await new userModel({name, email, phone, address, password:hashedPassword}).save()
+res.status(201).send({
+    success:true,
+    messange:"User Register successfully",
+    user,
+})
+}
+catch(error){
+console.log(error);
+res.status(500).send({
+    success:false,
+    messange: "error in Registrationa",
+    error,
+});
+    }
+};
+
+
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import colors from 'colors';
 import dotenv from  'dotenv';
 import morgan from 'morgan';
 import connectDB from './config/db.js';
+import authRoutes from './routes/authRoute.js';
 
 // configure env
 dotenv.config()
@@ -21,6 +22,8 @@ const app = express();
 app.use(express.json());
 app.use(morgan('dev'))
 
+// routes
+app.use('/api/v1/auth',authRoutes);
 
 //rest api
 
