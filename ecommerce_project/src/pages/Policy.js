008418856619fc57759e36519a86f40539a87e3a
unknown <J4sumit@gmail.com>
2023-05-06 16:21:35 +0530

    navigation menu created

--- ecommerce_project/src/pages/Policy.js
+++ ecommerce_project/src/pages/Policy.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Layout from '../components/Layout/Layout'
 
 const Policy = () => {
-  return (
-    <Layout>
-        <h1>Privacy Policy Page</h1>
+    return (
+        <Layout>
+            <h1>Privacy Policy Page</h1>
         </Layout>
-  )
+    )
 }
 
 export default Policy
