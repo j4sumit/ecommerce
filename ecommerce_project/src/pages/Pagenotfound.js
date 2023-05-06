0359c294cb3e834f5a1709b8933edaca04c546ad
unknown <J4sumit@gmail.com>
2023-05-06 23:22:50 +0530

    body part definded for the page not found

--- ecommerce_project/src/pages/Pagenotfound.js
+++ ecommerce_project/src/pages/Pagenotfound.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
 import Layout from '../components/Layout/Layout'
 
 const Pagenotfound = () => {
   return (
     <Layout>
-        <h1>Page Not Found </h1>
+      <div className='pnf'>
+      <h1 className="pnf-title">404</h1>
+      <h2 className='pnf-heading'>Ooops ! Page Not Found</h2>
+      <Link to="/" className="pnf-btn">Go Back</Link>
+      </div>
     </Layout>
-  )
-}
+  );
+};
 
 export default Pagenotfound
