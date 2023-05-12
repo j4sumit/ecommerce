dac0789e9e73c45b457ef02a3281234eda77cb28
unknown <J4sumit@gmail.com>
2023-05-12 18:18:28 +0530

    Model, route, controler created for products

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ import connectDB from './config/db.js';
 import authRoutes from './routes/authRoute.js';
 import cors from 'cors';
 import categoryRoutes from './routes/categoryRoutes.js';
+import productRoutes from "./routes/productRoutes.js";
+
+
 // configure env
 dotenv.config()
 
@@ -28,6 +31,7 @@ app.use(morgan('dev'))
 // routes
 app.use('/api/v1/auth',authRoutes);
 app.use('/api/v1/category',categoryRoutes);
+app.use('/api/v1/product', productRoutes);
 
 //rest api
 
