eae5fd5067a59ebb3d9f822eb7e63499acf32904
unknown <J4sumit@gmail.com>
2023-05-12 12:07:08 +0530

    Category API created for create operation

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import morgan from 'morgan';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoute.js';
 import cors from 'cors';
-
+import categoryRoutes from './routes/categoryRoutes.js';
 // configure env
 dotenv.config()
 
@@ -27,6 +27,7 @@ app.use(morgan('dev'))
 
 // routes
 app.use('/api/v1/auth',authRoutes);
+app.use('/api/v1/category',categoryRoutes);
 
 //rest api
 
@@ -41,3 +42,4 @@ const PORT = process.env.PORT || 8080;
     app.listen(PORT, ()=>{
         console.log(`server running on mode ${process.env.DEV_MODE} and PORT on ${PORT}`.bgCyan.white);
     })
+
